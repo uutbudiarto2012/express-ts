d3ffe473bc4389a04254d5664464aeb857f6dca4
uutbudiarto2012 <hikids.developer@gmail.com>
2023-12-27 23:27:18 +0700

    first commit

---
+++ notes.md
@@ -0,0 +1,16 @@
+```
+yarn add -D prettier typescript ts-node eslint @typescript-eslint/parser eslint-config-standard eslint-plugin-node eslint-plugin-standard nodemon
+
+
+yarn add -D  @types/express @types/dotenv
+
+yarn add -D husky pretty-quick
+```
+- setup eslint
+    npx eslint --init
+
+- setup ts
+    npx tsc --init
+
+- setup prettier
+    .prettierrc
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { type Application, type Response, type Request, type NextFunction } from 'express'
+import 'dotenv/config'
+const app: Application = express()
+const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hallo Duniaaa')
+})
+app.listen(port, () => {
+  console.log(`App run on port :  ${port}`)
+})
