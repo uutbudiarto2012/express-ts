b53ae33dba5b745624cadaf8bcb47f54d476f7e9
uutbudiarto2012 <hikids.developer@gmail.com>
2023-12-27 23:50:02 +0700

    -

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import express, { type Application, type Response, type Request, type NextFunction } from 'express'
+import express, {
+  type Application,
+  type Response,
+  type Request,
+  type NextFunction
+} from 'express'
 import 'dotenv/config'
 const app: Application = express()
-const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+const port: number =
+  process.env.PORT != null ? parseInt(process.env.PORT) : 3000
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hallo Duniaaa')
